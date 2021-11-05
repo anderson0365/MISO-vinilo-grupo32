157c58eea413af0b456fd3e6f26ed40449c42f02
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2021-11-05 17:31:36 -0500

    Detalle album: version 0

--- app/src/main/java/com/miso_vinilo_grupo32/MainActivity.kt
+++ app/src/main/java/com/miso_vinilo_grupo32/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.miso_vinilo_grupo32
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.user_button).setOnClickListener {
+            var intent = Intent(this, AlbumDetail::class.java)
+            intent.putExtra("albumId", 100)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/miso_vinilo_grupo32/brokers/VolleyBroker.kt
@@ -0,0 +1,29 @@
+package com.miso_vinilo_grupo32.brokers
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
+class VolleyBroker constructor(context: Context) {
+    val instance: RequestQueue = Volley.newRequestQueue(context.applicationContext)
+
+    companion object{
+        const val BASE_URL= "http://192.168.72.132:3000/"
+
+        fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+            return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+        }
+        fun postRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+            return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
+        }
+        fun putRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+            return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
+        }
+    }
+
+}
