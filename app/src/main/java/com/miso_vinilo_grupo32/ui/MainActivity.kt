9e665b091daccd5a4b1cc501de1f71922aae31a7
anderson0365 <al.caceres@uniandes.edu.co>
2021-11-07 17:03:18 -0500

    Feature/album detail (#49)
    * Detalle album: version 0
    
    * Configuracion inicial MVVM
    
    * Ajustes de visualización
    
    * Se agrega repository y se modifican estilos

---
+++ app/src/main/java/com/miso_vinilo_grupo32/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.miso_vinilo_grupo32.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.miso_vinilo_grupo32.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.user_button).setOnClickListener {
+            var intent = Intent(this, AlbumDetail::class.java)
+            intent.putExtra("albumId", 100)
+            startActivity(intent)
+        }
+    }
+}
